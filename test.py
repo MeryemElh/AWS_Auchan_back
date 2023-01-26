d3d854b7a9030b31a9d20bcc5e85ccd5211a0080
Meryem EL Halfa <71674839+MeryemElh@users.noreply.github.com>
2023-01-26 04:41:30 +0000

    Get data from db
    Radio (chart3) not implemented yet

--- test.py
+++ test.py
@@ -37,16 +37,17 @@ app.add_middleware(
 
 @app.get("/getChart1Data")
 async def getChart1Data():
-    return [{"brand": "AUCHAN BIO", "count": 5}, {"brand": "USHUAIA", "count": 2}]
     cursor = connection.cursor()
     cursor.execute("""  SELECT brand, COUNT(*)
                         FROM (  SELECT DISTINCT url, brand
                                 FROM product_data) as brand_products
                         GROUP BY brand
-                        ;""")
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 7 ;""")
+    elem_list = cursor.fetchall()
     cursor.close()
 
-    return [{'brand': elem[0], 'count': elem[1]} for elem in cursor.fetchall()]
+    return [{'brand': elem[0], 'count': elem[1]} for elem in elem_list]
 
 def get_parent(id: str, liste : List[Tuple[str]]):
     if not liste[id]["parent_id"] or not liste[liste[id]["parent_id"]]["parent_id"]:
@@ -57,7 +58,6 @@ def get_parent(id: str, liste : List[Tuple[str]]):
 
 @app.get("/getChart2Data")
 async def getChart2Data():
-    return [{"category": "Fruits et légumes frais", "count": 5}, {"category": "Hygiène, beauté, parapharmacie", "count": 2}]
     cursor = connection.cursor()
 
     cursor.execute("""  SELECT category_id, COUNT(*)
@@ -83,7 +83,7 @@ async def getChart2Data():
     big_categories_count = {}
     for cat_id, count in categories_count:
         parent_id = get_parent(cat_id, categories_full)
-        big_categories_count[parent_id] = big_categories_count.get(str(parent_id), 0) + count
+        big_categories_count[parent_id] = big_categories_count.get(parent_id, 0) + count
 
     return [{"category": categories_full[id]["name"], "count": count} for id, count in big_categories_count.items()]
 
@@ -103,26 +103,75 @@ async def getChart3Data():
 
 @app.get("/getChart4Data")
 async def getChart4Data():
-    return [{"category": "Fruits et légumes frais", "price": 5.14}, {"category": "Hygiène, beauté, parapharmacie", "price": 10.99}]
+
     cursor = connection.cursor()
-    cursor.execute("""SELECT* FROM product_data;""")
+    cursor.execute("""  SELECT category_id, AVG(price_unit)
+                        FROM (  SELECT DISTINCT ON (url) url, timestamp, category_id, price_unit::float
+                                FROM product_data
+                                WHERE price_unit is not null
+                                ORDER BY url, timestamp DESC) AS price_products
+                        GROUP BY category_id
+                        ;""")
+    categories_price_avg = cursor.fetchall()
+
+    cursor.execute("""  SELECT id, parent_category_id, name
+                        FROM category
+                        ;""")
+    categories = cursor.fetchall()
+    categories_full = {}
+    for cat in categories:
+        categories_full[cat[0]] = {
+            "parent_id": cat[1],
+            "name": cat[2]
+        }
+
+    cursor.close()
+
+    big_categories_avg = {}
+    for cat_id, avg in categories_price_avg:
+        parent_id = get_parent(cat_id, categories_full)
+        big_categories_avg[parent_id] = {
+            "sum": big_categories_avg.get(parent_id, {}).get("sum", 0) + avg,
+            "count": big_categories_avg.get(parent_id, {}).get("count", 0) + 1
+        }
+    for key in big_categories_avg.keys():
+        big_categories_avg[key] = big_categories_avg[key]["sum"]/big_categories_avg[key]["count"]
+
+    return [{"category": categories_full[id]["name"], "price": avg} for id, avg in big_categories_avg.items()]
 
 @app.get("/getNumber1")
 async def getNumber1():
-    return 7
     cursor = connection.cursor()
-    cursor.execute("""SELECT* FROM product_data;""")
+    cursor.execute("""SELECT COUNT(*) FROM (SELECT DISTINCT availability, url FROM product_data) AS prods;""")
+    res = cursor.fetchall()[0]
+    cursor.close()
+    return res[0] if res else -1
 
 @app.get("/getNumber2")
 async def getNumber2():
-    return 7
     cursor = connection.cursor()
-    cursor.execute("""SELECT* FROM product_data;""")
+    cursor.execute("""SELECT COUNT(*) FROM (SELECT DISTINCT availability, url FROM product_data) AS prods WHERE availability = true;""")
+    res = cursor.fetchall()[0]
+    cursor.close()
+    return res[0] if res else -1
 
 @app.get("/getNumber3")
 async def getNumber3():
-    return 6.81
     cursor = connection.cursor()
-    cursor.execute("""SELECT* FROM product_data;""")
+    cursor.execute("""  SELECT AVG(price_unit)
+                        FROM (  SELECT
+                                DISTINCT ON (url) url,
+                                timestamp,
+                                price_unit::float
+                            FROM
+                                product_data
+                            WHERE
+                                price_unit is not null
+                            ORDER BY
+                                url,
+                                timestamp DESC) AS prices;""")
+    res = cursor.fetchall()[0]
+    cursor.close()
+    return res[0] if res else -1
 
 uvicorn.run(app, host='0.0.0.0', port=80)
