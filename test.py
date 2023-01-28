26f837921446d629d46cd72f554d43dda75c4efe
Meryem EL Halfa <71674839+MeryemElh@users.noreply.github.com>
2023-01-28 14:18:26 +0000

    Optimized SQL queries

--- test.py
+++ test.py
@@ -61,32 +61,18 @@ def get_parent(id: str, liste : List[Tuple[str]]):
 async def getChart2Data():
     cursor = connection.cursor()
 
-    cursor.execute("""  SELECT category_id, COUNT(*)
+    cursor.execute("""  SELECT parent_category.name, COUNT(*)
                         FROM (  SELECT DISTINCT url, category_id
-                                FROM product_data) as brand_products
-                        GROUP BY category_id
+                                FROM product_data) as brand_products, category AS child_category, category AS parent_category
+                        WHERE child_category.id = category_id AND child_category.first_parent_category_id = parent_category.id
+                        GROUP BY parent_category.name
+                        ORDER BY COUNT(*) DESC
+                        LIMIT 6
                         ;""")
-    categories_count = cursor.fetchall()
 
-    cursor.execute("""  SELECT id, parent_category_id, name
-                        FROM category
-                        ;""")
-    categories = cursor.fetchall()
-    categories_full = {}
-    for cat in categories:
-        categories_full[cat[0]] = {
-            "parent_id": cat[1],
-            "name": cat[2]
-        }
-
-    cursor.close()
-
-    big_categories_count = {}
-    for cat_id, count in categories_count:
-        parent_id = get_parent(cat_id, categories_full)
-        big_categories_count[parent_id] = big_categories_count.get(parent_id, 0) + count
-
-    return [{"category": categories_full[id]["name"], "count": count} for id, count in big_categories_count.items()]
+    res = cursor.fetchall()
+    
+    return [{"category": elem[0], "count": elem[1]} for elem in res]
 
 
 @app.get("/getChart3Data")
@@ -96,6 +82,7 @@ async def getChart3Data():
     cursor.execute("""  SELECT brand, AVG(rating_value), AVG(price_unit), MAX(price_unit), MIN(price_unit), COUNT(*), COUNT(CASE WHEN availability THEN 1 END)
                         FROM (  SELECT DISTINCT ON (url, timestamp) url, timestamp, brand, rating_value, price_unit, availability
                                 FROM product_data
+                                WHERE brand IS NOT null
                                 ORDER BY url, timestamp DESC) AS data
                         GROUP BY brand
                         ORDER BY COUNT(*) DESC
@@ -104,52 +91,38 @@ async def getChart3Data():
     elem_list = cursor.fetchall()
     cursor.close()
 
-    return [{elem[0]: {
-                'nbr of products': elem[5],
-                'max price': elem[3],
-                'min price': elem[4],
-                'mean price': elem[2],
-                'nbr of available products': elem[6],
-                'rating': elem[1]
-            }} for elem in elem_list]
+    elem_dict = {}
+    for elem in elem_list:
+        elem_dict[elem[0]] = {
+            'name': elem[0],
+            'nbr_prod': elem[5],
+            'max': elem[3],
+            'min': elem[4],
+            'mean': elem[2],
+            'nbr_avail': elem[6],
+            'mean_rating': elem[1]
+        }
+
+    return elem_dict
 
 @app.get("/getChart4Data")
 async def getChart4Data():
 
     cursor = connection.cursor()
-    cursor.execute("""  SELECT category_id, AVG(price_unit)
+    cursor.execute("""  SELECT parent_category.name, AVG(price_unit)
                         FROM (  SELECT DISTINCT ON (url) url, timestamp, category_id, price_unit::float
                                 FROM product_data
                                 WHERE price_unit is not null
-                                ORDER BY url, timestamp DESC) AS price_products
-                        GROUP BY category_id
+                                ORDER BY url, timestamp DESC) AS price_products, category AS child_category, category AS parent_category
+                        WHERE child_category.id = category_id AND child_category.first_parent_category_id = parent_category.id
+                        GROUP BY parent_category.name
+                        ORDER BY AVG(price_unit) DESC
+                        LIMIT 6
                         ;""")
-    categories_price_avg = cursor.fetchall()
 
-    cursor.execute("""  SELECT id, parent_category_id, name
-                        FROM category
-                        ;""")
-    categories = cursor.fetchall()
-    categories_full = {}
-    for cat in categories:
-        categories_full[cat[0]] = {
-            "parent_id": cat[1],
-            "name": cat[2]
-        }
+    res = cursor.fetchall()
 
-    cursor.close()
-
-    big_categories_avg = {}
-    for cat_id, avg in categories_price_avg:
-        parent_id = get_parent(cat_id, categories_full)
-        big_categories_avg[parent_id] = {
-            "sum": big_categories_avg.get(parent_id, {}).get("sum", 0) + avg,
-            "count": big_categories_avg.get(parent_id, {}).get("count", 0) + 1
-        }
-    for key in big_categories_avg.keys():
-        big_categories_avg[key] = big_categories_avg[key]["sum"]/big_categories_avg[key]["count"]
-
-    return [{"category": categories_full[id]["name"], "price": avg} for id, avg in big_categories_avg.items()]
+    return [{"category": elem[0], "price": elem[1]} for elem in res]
 
 @app.get("/getNumber1")
 async def getNumber1():
